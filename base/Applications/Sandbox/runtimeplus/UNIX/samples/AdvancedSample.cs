86cb5536b72b7f4f698ec9a36baad7700f977227
Jonathan Moore <jdm7dv@gmail.com>
2016-11-23 17:58:06 -0500

    restructured

--- base/Applications/Sandbox/runtimeplus/UNIX/samples/AdvancedSample.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public class AdvancedSample
-{
-	public static void Main(string [] args)
-	{
-		Console.WriteLine ("Advanced Sample");
-	}
-}
--- base/Applications/Sandbox/runtimeplus/UNIX/samples/BasicSample.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public class BasicSample
-{
-	public static void Main(string [] args)
-	{
-		Console.WriteLine ("Basic Sample");
-	}
-}
